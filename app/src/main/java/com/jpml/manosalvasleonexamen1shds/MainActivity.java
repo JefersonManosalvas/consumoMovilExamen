a6d9e3740dda6bdc6fb74038d0ed384cc34baf33
JefersonManosalvas <jeferson.manosalvas979@ist17dejulio.edu.ec>
2024-12-09 09:28:57 -0500

    configuramos el codigo para asignar la url para el cunsumo de la api

--- app/src/main/java/com/jpml/manosalvasleonexamen1shds/MainActivity.java
+++ app/src/main/java/com/jpml/manosalvasleonexamen1shds/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jpml.manosalvasleonexamen1shds;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView respiuesta;
+    Button botonConsumir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        respiuesta=findViewById(R.id.textView_respuesta);
+        botonConsumir=findViewById(R.id.button_consumir);
+
+        botonConsumir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerServicioWeb("http://172.21.192.1:3001/mensaje");
+            }
+        });
+
+
+
+
+
     }
+
+    private void obtenerServicioWeb(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                respiuesta.setText(response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return new HashMap<>();
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+
+
+
+
+
 }
