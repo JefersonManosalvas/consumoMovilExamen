4dff22815623dd1989e5a2368e88a380805cba80
JefersonManosalvas <jeferson.manosalvas979@ist17dejulio.edu.ec>
2024-12-16 10:54:13 -0500

    cambios en clases

--- app/src/main/java/com/jpml/manosalvasleonexamen1shds/MainActivity.java
+++ app/src/main/java/com/jpml/manosalvasleonexamen1shds/MainActivity.java
@@ -3,6 +3,7 @@ package com.jpml.manosalvasleonexamen1shds;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,17 +18,22 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editext_ingresoDatos;
     TextView respiuesta;
-    Button botonConsumir;
+    Button botonConsumir, botnbiografia, botnsuma,botonsumaCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +46,35 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        editext_ingresoDatos=findViewById(R.id.etxt_ingDatos);
         respiuesta=findViewById(R.id.textView_respuesta);
         botonConsumir=findViewById(R.id.button_consumir);
+        botnbiografia=findViewById(R.id.button_biografia);
+        botnsuma=findViewById(R.id.button_suma);
+        botonsumaCliente=findViewById(R.id.button_sumaCliente);
 
         botonConsumir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                obtenerServicioWeb("http://172.21.192.1:3001/mensaje");
+                obtenerServicioWeb("http://10.10.30.142:3000/nombre");
+            }
+        });
+        botnbiografia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerServicioWebB("http://10.10.30.142:3000/jeferson");
+            }
+        });
+        botnsuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerServicioWebSuma("http://10.10.30.142:3000/suma");
+            }
+        });
+        botonsumaCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                obtenerServicioWebsumaCliente("http://10.10.30.142:3000/sumaCliente/"+editext_ingresoDatos.getText().toString());
             }
         });
 
@@ -57,10 +85,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void obtenerServicioWeb(String URL) {
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                respiuesta.setText(response);
+                try {
+                    JSONArray respuestaJson = new JSONArray(response);
+                    /*JSONObject respueta1 = respuestaJson.getJSONObject(0);
+                    JSONObject respueta2 = respuestaJson.getJSONObject(1);*/
+                    respiuesta.setText(respuestaJson.toString());
+                    /*respiuesta.setText(respueta1.toString()+"\n"+respueta2.toString());*/
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
 
             }
         }, new Response.ErrorListener() {
@@ -80,7 +117,73 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private void obtenerServicioWebB(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
+                    respiuesta.setText(response);
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return new HashMap<>();
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void obtenerServicioWebSuma(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                respiuesta.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return new HashMap<>();
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void obtenerServicioWebsumaCliente(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                respiuesta.setText("El resultado es: "+response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return new HashMap<>();
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 
 
 
